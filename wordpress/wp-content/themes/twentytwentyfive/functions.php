077dfafa74fc8808e73819e1decaffe8a3dc1a1b
shotaro_sakamoto <shotaro_sakamoto@totec.co.jp>
2025-08-18 10:21:16 +0900

    Add func - Allow SVG uploads

--- wordpress/wp-content/themes/twentytwentyfive/functions.php
+++ wordpress/wp-content/themes/twentytwentyfive/functions.php
@@ -159,3 +159,13 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
+
+// Allow SVG uploads
+function add_file_types_to_uploads($file_types)
+{
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes);
+	return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
